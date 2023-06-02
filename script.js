18b6db7714fa579b4ee169b01f0deac5017d08eb
Reon Kovalchuk <thesmollestlion@gmail.com>
2023-06-02 14:34:02 +0400

    новые домашки

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const main = document.getElementById('main');
+// export default function randomizer();
 function area(x1, y1, x2, y2) {
     sideA = Math.abs(x1 - x2);
     sideB = Math.abs(y1 - y2);
@@ -55,9 +55,11 @@ function randomizer(m, n) {
     return output;
 };
 
+
+
 function oddRandomizer() {
-    document.getElementById('input-m').value = Math.trunc(document.getElementById('input-m').value);
-    document.getElementById('input-n').value = Math.trunc(document.getElementById('input-n').value);
+    document.getElementById('input-m').value = document.getElementById('input-m').value;
+    document.getElementById('input-n').value = document.getElementById('input-n').value;
     let m = document.getElementById('input-m').value;
     let n = document.getElementById('input-n').value;
     let output = randomizer(m, n);
@@ -113,17 +115,12 @@ function NormalizeName() {
     });
     newName = newName.trim();
     document.getElementById('name').value = newName;
-    // if (name === newName) {
-    //     alert('Имя не менялось');
-    // } else {
-    //     alert('Имя изменено');
-    // }
 }
 
 function generateArray() {
-    document.getElementById('array-m').value = Math.trunc(document.getElementById('array-m').value);
-    document.getElementById('array-n').value = Math.trunc(document.getElementById('array-n').value);
-    document.getElementById('array-count').value = Math.trunc(document.getElementById('array-count').value);
+    document.getElementById('array-m').value = document.getElementById('array-m').value;
+    document.getElementById('array-n').value = document.getElementById('array-n').value;
+    document.getElementById('array-count').value = document.getElementById('array-count').value;
 
     let m = document.getElementById('array-m').value;
     let n = document.getElementById('array-n').value;
@@ -138,19 +135,16 @@ function generateArray() {
         output += `${parseInt(i) + 1}. ${randomNumber} \n`;
     }
 
-    document.getElementById('array-output').innerHTML = output;
+    document.getElementById('array-output').value = output;
 }
 
 
 
 function stringTurn() {
     let input = document.getElementById('string-input').value;
-    let output = '';
-    for (let i = input.length - 1; i > -1; i--) {
-        output += input[i];
-    };
 
-    document.getElementById('string-output').value = output;
+
+    document.getElementById('string-output').value = input.split('').reverse().join('');
 }
 
 var minesSpan = document.getElementsByClassName('mine');
@@ -213,3 +207,163 @@ function showMissionStatus(tankHP) {
     missionStatus.innerHTML = message;
     missionStatus.classList.add('show');
 }
+
+function findObjectByKeyAndValue(array, searchKey, searchValue) {
+    let outputArray = new Array();
+    array.forEach(element => {
+        for (const [key, value] of Object.entries(element)) {
+            if (key == searchKey && value == searchValue) {
+                let outputObj = new Object();
+                Object.assign(outputObj, element)
+                // outputObj[searchKey] = searchValue;
+                outputArray.push(outputObj);
+            }
+        }
+    });
+    return outputArray;
+}
+
+function setup() {
+    let objects = [
+        { name: 'Василий', surname: 'Васильев' },
+        { name: 'Иван', surname: 'Иванов' },
+        { name: 'Пётр', surname: 'Петров' },
+        { name: 'Василий', surname: 'Петров' },
+    ]
+    searchResult = findObjectByKeyAndValue(objects, 'name', 'Василий');
+    console.log('Найдены результаты: \n')
+    searchResult.forEach(element => {
+        for (const [key, value] of Object.entries(element)) {
+            console.log(`${key}: ${value}`);
+        }
+    });
+}
+
+
+
+function getOptionsArray() {
+    let optionsArray = [
+        1,
+        'two',
+        new Date(),
+        { value: 'choose', label: 'выбери' },
+        { value: 'your', label: 'своего' },
+        { value: 'fighter', label: 'бойца' },
+    ]
+    let newOptionsArray = [];
+
+    optionsArray.forEach(element => {
+        if ((typeof element === 'number')
+            || (typeof element === 'string')
+            || (element instanceof Date)) {
+            newOptionsArray.push({ value: element, label: element })
+        }
+        else {
+            newOptionsArray.push(element)
+        }
+    });
+    return newOptionsArray;
+
+}
+
+// переменная, которая отслеживает, сколько секунд прошло
+var nowSeconds = 0;
+var intervalVariable;
+var startBtn = document.querySelector('.timer-start');
+var pauseBtn = document.querySelector('.timer-pause');
+
+function startTimer() {
+    let timerMinutes = parseInt(document.querySelector('.timer-minutes').value);
+    let timerSeconds = parseInt(document.querySelector('.timer-seconds').value);
+
+    if (nowSeconds == 0 && (timerMinutes != 0 || timerSeconds != 0)) {
+
+        console.log('запустить таймер с нуля');
+        document.querySelector('.minutes').textContent = timerMinutes;
+        document.querySelector('.seconds').textContent = timerSeconds;
+        nowSeconds = timerMinutes * 60 + timerSeconds;
+        renderTimer();
+    } else if (nowSeconds = 0 && timerMinutes == 0 && timerSeconds == 0) {
+        return;
+    }
+
+    startBtn.classList.toggle('hidden');
+    pauseBtn.classList.toggle('hidden');
+    intervalVariable = setInterval(timerTick, 1000);
+
+}
+
+function pauseTimer() {
+    //pause timer
+    startBtn.classList.toggle('hidden');
+    pauseBtn.classList.toggle('hidden');
+    clearInterval(intervalVariable);
+}
+
+function stopTimer() {
+    //stop timer
+    nowSeconds = 0;
+    document.querySelector('.minutes').textContent = '00';
+    document.querySelector('.seconds').textContent = '00';
+    startBtn.classList.toggle('hidden');
+    pauseBtn.classList.toggle('hidden');
+    clearInterval(intervalVariable);
+}
+
+function secondsToTime(seconds) {
+    // var h = parseInt(seconds / 3600 % 24);
+    var m = parseInt(seconds / 60 % 60);
+    var s = parseInt(seconds % 60);
+    return { 'minutes': leadZero(parseInt(m)), 'seconds': leadZero(parseInt(s)) };
+}
+
+function leadZero(num) {
+    var s = "" + num;
+    if (s.length < 2) {
+        s = "0" + s;
+    }
+    return s;
+}
+
+function renderTimer() {
+    var timer_nums = secondsToTime(nowSeconds);
+    document.querySelector('.minutes').textContent = timer_nums.minutes;
+    document.querySelector('.seconds').textContent = timer_nums.seconds;
+}
+
+function timerTick() {
+    nowSeconds--;
+    renderTimer();
+    if (nowSeconds == 0) {
+        stopTimer();
+    }
+
+
+
+}
+
+(function () {
+    let form = document.getElementById('select');
+    let select = document.createElement('select');
+    select.className = 'form-select';
+    form.insertAdjacentElement('beforeend', select);
+    let defaultOption = document.createElement('option');
+    defaultOption.textContent = 'Эти опции были созданы с помощью JS';
+    select.insertAdjacentElement('beforeend', defaultOption)
+    let optionsArray = getOptionsArray();
+    optionsArray.forEach(element => {
+        let option = document.createElement('option');
+        option.value = element.value;
+        option.textContent = element.label;
+        select.insertAdjacentElement('beforeend', option)
+    });
+
+})();
+
+$('body').on('input', '.input-number', function () {
+    console.log(typeof this.value);
+    // let stringValue = toString(this.value);
+    console.log(this.value.replace(/[^\d.]/g, ''));
+    this.value = parseInt(this.value.replace(/[^\d.]/g, ''));
+
+});
